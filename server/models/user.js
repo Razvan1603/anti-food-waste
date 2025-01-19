99a6d516ddfd31a236fcbd8da620acf11790668c
ciuntudan <dan.ciuntu@yahoo.ro>
2025-01-19 15:30:15 +0200

    user

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
+const groupSchema = new mongoose.Schema({
+  name: { type: String, required: true }, 
+  members: [
+    {
+      username: { type: String, required: true }, 
+      label: { type: String }, 
+    },
+  ],
+});
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  friends: [{ username: { type: String, required: true } }], 
+  groups: [groupSchema], 
 });
 
 module.exports = mongoose.model('User', userSchema);
