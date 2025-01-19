78d7ff1a4ed26736092dc2a937a7ab6f44fec8e7
ciuntudan <dan.ciuntu@yahoo.ro>
2025-01-19 02:22:29 +0200

    fix add friend

--- server/models/friend.js
+++ server/models/friend.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const friendSchema = new mongoose.Schema({
   username: { type: String, required: true }, 
-  owner: { type: String, required: true },  
+  owner: { type: String, required: true },   
 });
 
 module.exports = mongoose.model('Friend', friendSchema);
